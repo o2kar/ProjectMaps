1032965e7dfff05ecd150af75aa2d8d2053f21f3
cschroeter <christian.schroeter@1und1.de>
2012-06-27 11:01:44 +0200

    Added geolocation function

--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -1,8 +1,9 @@
-/* Author:
+/* Author: Christian Schröter
+ * Ermittelt die aktuelle Position des Users
+ */
 
-*/
-
-
-
-
-
+navigator.geolocation.getCurrentPosition(function(position) {
+	document.getElementById('geolocation').innerHTML = 'Latitude: ' + position.coords.latitude + ' / Longitude: ' + position.coords.longitude;
+}, function() {
+	document.getElementById('geolocation').innerHTML = 'Deine Position konnte leider nicht ermittelt werden';
+});
